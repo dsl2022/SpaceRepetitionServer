7269f175ebcb68281b866760673e983bc5774a33
Eddie Bueno <eddiebueno93@gmail.com>
2019-05-29 12:59:13 -0700

    added body parser for language router

--- src/language/language-router.js
+++ src/language/language-router.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const LanguageService = require('./language-service')
 const { requireAuth } = require('../middleware/jwt-auth')
+const jsonBodyParser = express.json();
 
 const languageRouter = express.Router()
 
@@ -58,10 +59,12 @@ languageRouter
   })
 
 languageRouter
-  .post('/guess', async (req, res, next) => {
+  .route('/guess')
+  .post(jsonBodyParser, async (req, res, next) => {
     // implement me
     const userId = req.user.id;
-    const guess = req.body;
+    
+    console.log(req.body);
     const words = await 
     LanguageService.getLanguageWords(
       req.app.get('db'),
@@ -71,15 +74,39 @@ languageRouter
     LanguageService.getLanguageLinkedList(
       words
     )
-    console.log(req.body);
-    console.log(ll.head.data.translation)
-    if (req.body === undefined){
+    if (!req.body.guess){
       return res.status(400).json({error:`Missing 'guess' in request body`})
     }
-    const guess = req.guess;
+    const guess = req.body.guess;
+    const correct = guess === ll.head.data.translation; 
+
+    if (correct){
+      // increase correct count for word
+      // update total score
+    }else{
+      // increase incorrect count for word
+    }
+    // setM
     
+    // persist updated words and language in db
 
-
+    // send response
+    // correct :{
+    //   "nextWord": "test-next-word-from-correct-guess",
+    //   "wordCorrectCount": 111,
+    //   "wordIncorrectCount": 222,
+    //   "totalScore": 333,
+    //   "answer": "test-answer-from-correct-guess",
+    //   "isCorrect": true
+    // }
+    // incorrect: {
+    //   "nextWord": "test-next-word-from-incorrect-guess",
+    //   "wordCorrectCount": 888,
+    //   "wordIncorrectCount": 111,
+    //   "totalScore": 999,
+    //   "answer": "test-answer-from-incorrect-guess",
+    //   "isCorrect": false
+    // }
 
     res.send('implement me!')
   })
